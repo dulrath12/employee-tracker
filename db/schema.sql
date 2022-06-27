ed769ec8d5d92f25b1b4fcb231652f521629a80f
Dakotah Ulrath <dulrath@live.com>
2022-06-26 22:27:33 -0400

    remove department, role, employee, and view salary

--- db/schema.sql
+++ db/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE role (
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE CASCADE
+    CONSTRAINT foreign_key_department FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -23,6 +23,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT NULL,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(role_id) ON DELETE CASCADE,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(employee_id) ON DELETE SET NULL
+    CONSTRAINT foreign_key_role FOREIGN KEY (role_id) REFERENCES role(role_id) ON DELETE CASCADE,
+    CONSTRAINT foreign_key_manager FOREIGN KEY (manager_id) REFERENCES employee(employee_id) ON DELETE SET NULL
 );
